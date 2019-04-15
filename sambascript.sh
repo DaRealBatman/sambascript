e284fd5423e7da225ad96629732daa233751fa42
oberuaz <oberuaz@gmail.com>
2019-04-15 16:41:31 +0200

    szoveg2

--- sambascript.sh
+++ sambascript.sh
@@ -7,6 +7,15 @@ mkdir /srv/kozos
 chmod 777 /srv/kozos
 mkdir /srv/readonly
 chmod 755 /srv/readonly
-
-
-mv-vel átnevezzük a régit, cp-vel odamásoljuk a sajátot
+mv /etc/samba/smb.conf /etc/samba/smb.conf.old
+cp ./smb.conf /etc/samba/smb.conf
+service smbd restart
+testparm
+sudo adduser user2 --gecos "user2,,," --disabled-password
+echo "user2:user2jelszo" | sudo chpasswd
+mkdir /srv/user2
+chown user2 /srv/user2
+chmod 700 /srv/user2
+# felhasználó felvétele kézzel (sudo smbpasswd -a user2, majd bekéri a jelszót)
+# sudo service smbd restart
+# teszt kliensről: smb://ipcim/user2
